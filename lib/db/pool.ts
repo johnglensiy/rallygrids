aa0925462ccc8bb2c5feae8a3d45d852616640ce
Glenn Siy <johnglen_siy@berkeley.edu>
2026-09-24 22:34:44 -0700

    fix: db pool to use env connection string

--- lib/db/pool.ts
+++ lib/db/pool.ts
@@ -3,9 +3,5 @@ import "server-only";
 import { Pool } from "pg";
 
 // eventually this will be hosted
-export const pool = new Pool({
-  host: "localhost",
-  user: "johnglen.siy",
-  database: "rally_grids",
-  port: 5432,
-});
+const connectionString = process.env.DATABASE_URL;
+export const pool = new Pool({ connectionString });
